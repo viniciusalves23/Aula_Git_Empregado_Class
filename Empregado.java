e1f3cbed7119673af9f4f86b9f08f1889c758ff9
Vinicius <Viniciussouzaalves@gmail.com>
2022-08-15 12:04:13 -0300

    Incluindo arquivos faltantes do commit anterior

--- Empregado.java
+++ Empregado.java
@@ -32,4 +32,11 @@ public class Empregado{
     public void setIdade (int idade){
     this.idade = idade;
     }
+    public void setTipo (int tipo){
+        this.tipo = tipo;
+    }
+    public int tipo (){
+        return this.tipo;
+    }
+       
    }
